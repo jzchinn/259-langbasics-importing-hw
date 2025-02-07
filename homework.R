357a74b8929369b8f8bb2a0354840ae354ab779c
macmonted <mmont168@ucr.edu>
2025-02-07 11:11:25 -0800

    Graded homework.R

--- homework.R
+++ homework.R
@@ -131,6 +131,22 @@ ds6 <- ds6 %>%
 
 # ANSWER
 
+###Key: 
+ds <- read_tsv(fnames, skip = 7, col_names = col_names, col_types = "iccl", id = "filename")
+
+# How to get more useful info out of file name?
+library(tidyr)
+ds <- ds %>% extract(filename, into = c("id","session"), "(\\d{4})_(\\d{1})") 
+#Extract takes a character variable, names of where to put the extracted data,
+# and then a regular expression saying what pattern to look for.
+# each part in parentheses is one variable to extract
+# \\d{4} means 4 digits, \\d{1} means 1 digit
+
+# Or use "separate", which breaks everything by any delimiter (or a custom one)
+# data_A/6191_1.txt will turn into:
+# data   A   6191   1   txt
+# if we only want to keep 6191 and 1, we can put NAs for the rest
+ds <- ds %>% separate(filename, into = c(NA, NA, "id", "session", NA))
 
 ### QUESTION 8 -----
 
@@ -139,4 +155,10 @@ ds6 <- ds6 %>%
 # There are two sheets of data -- import each one into a new tibble
 
 # ANSWER
+
+###Key: 
+install.packages("readxl")
+library(readxl)
+ppt_info <- read_xlsx("data_B/participant_info.xlsx")
+test_dates <- read_xlsx("data_B/participant_info.xlsx", col_names = c("participant", "test_date"), sheet = 2)
 
